08d530434b59b6a54b8108e718bf74eeed2915e6
brkp <brkp@tutanota.com>
2022-09-19 00:19:06 +0300

    initial implementation of the `Context::run` function

--- src/core/context.rs
+++ src/core/context.rs
@@ -1,5 +1,5 @@
-use crate::core::Window;
-use crate::Texture;
+use crate::{Trans, Texture, core::Window};
+use crate::core::state::{State, StateManager};
 
 pub struct ContextBuilder {
     pub(crate) width: usize,
@@ -16,6 +16,7 @@ pub struct ContextBuilder {
 }
 
 pub struct Context {
+    pub config: ContextBuilder,
     pub window: Window,
     pub texture: Texture,
 }
@@ -40,16 +41,51 @@ impl ContextBuilder {
         self
     }
 
-    pub fn build(&mut self) -> Context {
-        todo!()
+    pub fn build(self) -> Result<Context, String> {
+        Context::new(self)
     }
 }
 
 impl Context {
-    pub fn new(config: &ContextBuilder) -> Result<Self, String> {
-        let window = Window::new(config)?;
+    pub fn new(config: ContextBuilder) -> Result<Self, String> {
+        let window = Window::new(&config)?;
         let texture = Texture::new(config.width, config.height);
 
-        Ok(Self { window, texture })
+        Ok(Self {
+            config,
+            window,
+            texture,
+        })
+    }
+
+    pub fn run(&mut self, initial_state: Box<dyn State>) {
+        let mut state_manager = StateManager::with(initial_state);
+        // assuming that there is no other instance of `EventPump`
+        let mut event_pump = self.window.sdl.event_pump().unwrap();
+
+        'outer: while state_manager.get_state_count() > 0 {
+            let state = state_manager.get_active_state().unwrap();
+            for event in event_pump.poll_iter() {
+                match state.handle_event(self, event) {
+                    Trans::Pop => {
+                        state_manager.pop_state();
+                        continue 'outer;
+                    }
+                    Trans::Set(new_state) => {
+                        state_manager.set_state(new_state);
+                        continue 'outer;
+                    }
+                    Trans::Push(new_state) => {
+                        state_manager.push_state(new_state);
+                        continue 'outer;
+                    }
+                    Trans::Quit => break 'outer,
+                    Trans::None => ()
+                }
+            }
+
+            state.update(self);
+            state.draw(self);
+        }
     }
 }
