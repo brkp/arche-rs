6e6eb2583c2de52048a663547c712c608ee2fe4f
brkp <brkp@tutanota.com>
2022-09-18 23:01:54 +0300

    initial `StateManager` implementation

--- src/core/state.rs
+++ src/core/state.rs
@@ -1,9 +1,15 @@
+use sdl2::event::Event;
 use crate::core::context::Context;
 
 pub trait State {
-    fn handle_events(&mut self, ctx: &mut Context) -> Trans;
+    fn handle_event(&mut self, ctx: &mut Context, event: Event) -> Trans;
     fn update(&mut self, ctx: &mut Context);
     fn draw(&mut self, ctx: &mut Context);
+    // TODO:
+    // on start
+    // on stop
+    // on pause
+    // on resume
 }
 
 pub enum Trans {
@@ -11,5 +17,44 @@ pub enum Trans {
     Pop,
     Set(Box<dyn State>),
     Push(Box<dyn State>),
-    Quit
+    Quit,
+}
+
+pub struct StateManager {
+    stack: Vec<Box<dyn State>>,
+}
+
+impl StateManager {
+    pub fn new() -> Self {
+        Self {
+            stack: Vec::new(),
+        }
+    }
+
+    pub fn with(initial_state: Box<dyn State>) -> Self {
+        Self {
+            stack: vec![initial_state],
+        }
+    }
+
+    pub fn pop_state(&mut self) {
+        self.stack.pop();
+    }
+
+    pub fn set_state(&mut self, state: Box<dyn State>) {
+        self.stack.pop();
+        self.stack.push(state);
+    }
+
+    pub fn push_state(&mut self, state: Box<dyn State>) {
+        self.stack.push(state);
+    }
+
+    pub fn get_active_state(&mut self) -> Option<&mut Box<dyn State>> {
+        self.stack.last_mut()
+    }
+
+    pub fn get_state_count(&self) -> usize {
+        self.stack.len()
+    }
 }
