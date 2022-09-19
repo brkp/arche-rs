3403c211130eeb2a47623772b897edeced0b2bd1
brkp <brkp@tutanota.com>
2022-09-19 22:38:52 +0300

    not so pretty but complete implementation of the line drawing algorithm

--- src/graphics/draw/line.rs
+++ src/graphics/draw/line.rs
@@ -1,24 +1,40 @@
 use crate::{Color, Context, Texture};
 
-// TODO: implement a point type
-// TODO: vertical lines will cause a `panic`
 pub fn draw(ctx: &mut Context, x0: i32, y0: i32, x1: i32, y1: i32, color: Color) {
-    let mut x0 = x0;
-    let mut x1 = x1;
-    let mut y0 = y0;
-    let mut y1 = y1;
+    let mut x0 = x0 as f32;
+    let mut x1 = x1 as f32;
+    let mut y0 = y0 as f32;
+    let mut y1 = y1 as f32;
 
-    if x0 > x1 {
-        std::mem::swap(&mut x0, &mut x1);
-        std::mem::swap(&mut y0, &mut y1);
+    let dx = x0 - x1;
+    let dy = y0 - y1;
+
+    if dx.abs() > dy.abs() {
+        if x0 > x1 {
+            std::mem::swap(&mut x0, &mut x1);
+            std::mem::swap(&mut y0, &mut y1);
+        }
+
+        let m = dy/dx;
+        let mut y = y0;
+
+        for x in x0 as i32..=x1 as i32 {
+            ctx.texture.set_pixel(x as usize, y as usize, color);
+            y += m;
+        }
     }
+    else {
+        if y0 > y1 {
+            std::mem::swap(&mut x0, &mut x1);
+            std::mem::swap(&mut y0, &mut y1);
+        }
 
-    let a = (y1 - y0) / (x1 - x0);
-    let b = y0 - a * x0;
+        let m = dx/dy;
+        let mut x = x0;
 
-    for x in x0..x1 {
-        let x = x as i32;
-        let y = a*x + b;
-        ctx.texture.set_pixel(x as usize, y as usize, color);
+        for y in y0 as i32..=y1 as i32 {
+            ctx.texture.set_pixel(x as usize, y as usize, color);
+            x += m;
+        }
     }
 }
